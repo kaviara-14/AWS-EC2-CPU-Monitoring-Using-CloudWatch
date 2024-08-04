11f6c8c2327e0195fa1d34b5eeb154263d173b91
Kaviarasu M <kaviarasuvpl14@gmail.com>
2024-08-04 16:10:06 +0530

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -5,10 +5,10 @@ def lambda_handler(event, context):
 
     client = boto3.client('sns')
     snsArn = 'your ARN'
-    message = "Dear Team, This is to inform you that the CPU utilization has reached 50%. While this level is not critical, it’s advisable to monitor the situation to ensure there are no underlying issues affecting performance."
+    message = "Dear Team, This is to inform you that the CPU utilization has reached 65%. While this level is not critical, it’s advisable to monitor the situation to ensure there are no underlying issues affecting performance."
 
     response = client.publish(
         TopicArn = snsArn,
         Message = message ,
-        Subject='"Alert: CPU Utilization reached 50%"'
+        Subject='"Alert: CPU Utilization reached 65%"'
     )
